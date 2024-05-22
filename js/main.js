1e0d8b9744bad1a9a1e352a945d6d250f10a86b2
Fernando Canitrot <canitrot.fernando@gmail.com>
2024-05-22 19:18:31 -0300

    Organizador del dia

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// Organizador del dia
+
+// REGLAS
+// No te puedes pasar mas que las 24 horas del dia en hacer todas tus actividades.
+// Donde si o si tienes que dormir 8 horas. ¡¡PARA ESTAR AL 100!!
+// Tambien tus 8 horas de trabajo (9am a 17pm).
+// Y estudiar unas 2 horas.
+alert("RING RINGGG son las 8 am") 
+
+const dormir = 8
+const trabajo = 8
+const estudio = 2
+const desayunar = 0.50
+const cambiarte = 0.50
+const viaje_auto = 0.25
